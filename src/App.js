65aef0922bf4ad6d4279ee5bb2b1a36d0281eacc
Mohammad Shariq <shariqsway@gmail.com>
2023-02-13 23:09:45 +0530

    Create the component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
-      Navbar
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
