7dcca57d08daf80f75e709de7aa490da0d568c10
Mohammad Shariq <shariqsway@gmail.com>
2023-02-15 02:14:57 +0530

    Implement the search functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "../components";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography
+        variant="h4"
+        fontWeight={600}
+        fontFamily="Lexend Deca"
+        letterSpacing={-0.8}
+        mb={2}
+        sx={{
+          color: "#fff",
+        }}
+      >
+        Search results for{" "}
+        <span style={{ color: "#eb161e" }}>{searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
